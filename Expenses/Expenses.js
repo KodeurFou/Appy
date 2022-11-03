d995d90f5f3cad4ae6a5d08efbbb9cf67e7b15a7
Adiman007 <adiman.bonnefoy@gmail.com>
2022-11-03 22:46:37 +0100

    3/11

--- Expenses/Expenses.js
+++ Expenses/Expenses.js
@@ -1,47 +1,64 @@
 
 //const GroupUser = ;
-const GroupUserTemp = [{"name":"Arthur","exp":82},{"name":"Thomas","exp":17},{"name":"Rat","exp":0}]
+const GroupUsers = [{"name":"Arthur","exp":82},{"name":"Thomas","exp":17},{"name":"Rat","exp":0}]
 function GetTotal(){
     var total=0;
-    for (let user of GroupUserTemp){
+    for (let user of GroupUsers){
         total+=user.exp;
     }
-    console.log(total)
     return total
 }
 function SetBalance(total){
-    totalPerUser=total/GroupUserTemp.length
+    totalPerUser=total/GroupUsers.length
 
-    for (let user of GroupUserTemp){
+    for (let user of GroupUsers){
         var balance=0;
         balance=user.exp-totalPerUser;
         user["balance"]=balance;
-        console.log(user.balance)
     }
 }
 function YourCut(){
     PosUser=[];
-
-    for (let user of GroupUserTemp){
+    result=[];
+    for (let user of GroupUsers){
         if(user.balance>0){
             PosUser.push(user)
         }
     }
-    while (PosUser.length>0){
-        for (let user of GroupUserTemp){
-            if(user.balance===0 || user in PosUser){
+
+    for (let PUser of PosUser){
+        for (let user of GroupUsers){
+            if(user.balance>=0){
                 continue;
             }
             else{
-
+                let temp=0;
+                line ="";
+                if (user.balance >= PUser.balance){
+                    temp=PUser.balance;
+                    user.balance=user.balance+temp;
+                    PUser.balance=0;
+                    line +=user.name +" doit " + temp +" à "+PUser.name;
+                    result.push(line);
+                    break;
+                }
+                else{
+                    temp=-user.balance;
+                    user.balance=0;
+                    PUser.balance+=-temp;
+                    line +=user.name +" doit " + temp +" à "+PUser.name;
+                    result.push(line);
+                }
             }
         }
     }
+    return result;
 }
 function main(){
     total=GetTotal();
     SetBalance(total);
-    YourCut();
+    transaction =YourCut();
+    console.log(transaction)
 
 }
 main();
